043e6edc2161f1f254d962c64b67c62ba282692d
seong su kim <know.warehouse02@gmail.com>
2025-07-17 23:32:01 +0900

    404 not found styling

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,11 +1,20 @@
 import Link from 'next/link';
+import style from './404.module.css';
 
 export default function NotFound() {
   return (
-    <>
-      <h1>404</h1>
-      <p>Page not found</p>
-      <Link href="/">Go to home</Link>
-    </>
+    <main className={style.not__main}>
+      <h1 className={style.not__h1}>404 - Page Not Found</h1>
+      <p className={style.not__text}>
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <p className={style.not__text}>
+        You can go back to the
+        <br />
+        <button className={style.not__button}>
+          <Link href="/">home page</Link>
+        </button>
+      </p>
+    </main>
   );
 }
