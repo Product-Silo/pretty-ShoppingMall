7813194942e21413d987e3319b3af290f09fc0b0
seong su kim <know.warehouse02@gmail.com>
2025-07-19 20:16:34 +0900

    한글화

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -6,13 +6,13 @@ export default function NotFound() {
     <main className={style.not__main}>
       <h1 className={style.not__h1}>404 - Page Not Found</h1>
       <p className={style.not__text}>
-        Sorry, the page you are looking for does not exist.
+        죄송합니다, 찾고 계신 페이지가 존재하지 않습니다.
       </p>
       <p className={style.not__text}>
-        You can go back to the
+        버튼을 누르면 홈화면으로 돌아갑니다.
         <br />
         <button className={style.not__button}>
-          <Link href="/">home page</Link>
+          <Link href="/">홈화면으로 돌아가기</Link>
         </button>
       </p>
     </main>
